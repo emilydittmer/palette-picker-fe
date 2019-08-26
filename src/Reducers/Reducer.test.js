b5da3bd6290c72f7dd5abb974654b13333068b95
RyanFlachman <ryan@flachman.net>
2019-08-26 17:46:49 -0600

    Add ErrorReducer tests

---
+++ src/Reducers/Reducer.test.js
@@ -0,0 +1,25 @@
+import { ErrorReducer } from './ErrorReducer'
+
+
+describe('Reducers', () => {
+
+  describe('ErrorReducer', () => {
+    it('Should return default state', () => {
+      const mockState = undefined;
+      const expected = '';
+      const result = ErrorReducer(mockState, {})
+
+      expect(result).toEqual(expected)
+    })
+    it('Should return error message when action type is HAS ERRORED', () => {
+      const mockAction = {
+        type: 'HAS ERRORED',
+        error: 'Mock error message'
+      }
+      const expected = 'Mock error message'
+      const result = ErrorReducer('', mockAction)
+      
+      expect(result).toEqual(expected)
+  })
+})
+})
