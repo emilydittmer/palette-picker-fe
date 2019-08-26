766eb292e280da0857e6c724681a9af414e0ab61
RyanFlachman <ryan@flachman.net>
2019-08-26 12:44:39 -0600

    Add loading and error reducer to store

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux'
 import { ProjectReducer } from './ProjectReducer'
 import { PaletteReducer } from './PaletteReducer'
+import { LoadingReducer } from './LoadingReducer'
+import { ErrorReducer } from './ErrorReducer'
 
 const rootReducer = combineReducers({
   projects: ProjectReducer,
-  currentPalette: PaletteReducer
+  currentPalette: PaletteReducer,
+  error: ErrorReducer,
+  isLoading: LoadingReducer
 })
 
 export default rootReducer;
