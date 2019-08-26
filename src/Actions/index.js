ffbe140cab2df2ea782d3a3187876122c23c1c20
RyanFlachman <ryan@flachman.net>
2019-08-26 12:36:23 -0600

    Add error and loading actions

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -7,3 +7,13 @@ export const getAllProjects = projects => ({
   type: 'GET PROJECTS',
   projects
 })
+
+export const hasErrored = error => ({
+  type: 'HAS ERRORED',
+  error
+})
+
+export const isLoading = loading => ({
+  type: 'IS LOADING',
+  loading
+})
